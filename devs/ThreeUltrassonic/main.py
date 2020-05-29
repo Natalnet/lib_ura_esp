720fffd79ad37eb6d5db1cd1e80680a081d1ec74
Orivaldo Santana Jr <orivaldo@gmail.com>
2020-05-29 11:15:01 -0300

    Teste inicial para uso de 3 sensores ultrassons. Ainda não funciona

--- devs/ThreeUltrassonic/main.py
+++ devs/ThreeUltrassonic/main.py
@@ -1,15 +1,6 @@
-from hcsr04 import HCSR04
-import utime
+#from hcsr04 import HCSR04
+#import utime
 
-sensorF = HCSR04(trigger_pin=16, echo_pin=0)
+#sensorF = HCSR04(trigger_pin=16, echo_pin=0)
 
-while True:
-    distance = sensor.distance_cm()
-    if distance<30:
-        print('Vire para Direita')
-        dr.turnLeft()
-    else:
-        print('Siga em Frente')
-        dr.forward()
-    print('Distance:', distance, 'cm')
-    utime.sleep_ms(50)
+#
--- devs/ThreeUltrassonic/test.py
+++ devs/ThreeUltrassonic/test.py
@@ -1,8 +1,15 @@
 from hcsr04 import HCSR04
 import utime
 
-sensorF = HCSR04(trigger_pin=16, echo_pin=0)
+sensorF = HCSR04(trigger_pin=14, echo_pin=12)
+sensorL = HCSR04(trigger_pin=13, echo_pin=15)
+#sensorR = HCSR04(trigger_pin=16, echo_pin=34)
 
 distance = sensorF.distance_cm()
-
 print('Front:', distance, 'cm')
+
+distance = sensorL.distance_cm()
+print('Left:', distance, 'cm')
+
+#distance = sensorR.distance_cm()
+#print('Right:', distance, 'cm')
