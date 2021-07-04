540578bcb0dc535c8c02257c4433da98146782cc
Orivaldo Santana Jr <orivaldo@gmail.com>
2021-07-04 19:29:09 -0300

    Teste apenas para inscrição em um tópico MQTT

---
+++ ESP32/MQTTSubscribe/boot.py
@@ -0,0 +1,48 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+import json 
+conf_file = open('conf.json') 
+conf = json.load(conf_file) 
+conf_file.close() 
+
+
+ssid = conf["ssid"]
+password = conf["password"]
+mqtt_server = conf["mqtt_server"]
+server_port = conf["server_port"] 
+mqtt_user = conf["mqtt_user"]
+mqtt_password = conf["mqtt_password"]
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'test_mqtt/input'
+ 
+
+
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+
+
+
+
+
+
---
+++ ESP32/MQTTSubscribe/main.py
@@ -0,0 +1,34 @@
+# Complete project details at https://RandomNerdTutorials.com
+def sub_cb(topic, msg):
+  global topic_sub
+  print((topic, msg))
+
+
+def connect_and_subscribe():
+  global client_id, mqtt_server, topic_sub, server_port, mqtt_user, mqtt_password
+  client = MQTTClient(client_id, mqtt_server, server_port, mqtt_user, mqtt_password)
+  client.set_callback(sub_cb)
+  client.connect()
+  client.subscribe(topic_sub)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
+
+try:
+  client = connect_and_subscribe()
+except OSError as e:
+  restart_and_reconnect()
+
+while True:
+  try:
+    client.check_msg()
+  except OSError as e:
+    restart_and_reconnect()
+
+
+
+
