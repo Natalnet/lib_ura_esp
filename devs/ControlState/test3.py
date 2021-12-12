f63074ad88c857cc0da93bff426afa5f68806205
Orivaldo Santana Jr <orivaldo@gmail.com>
2021-12-11 23:12:31 -0300

    teste 3 ok

--- devs/ControlState/test3.py
+++ devs/ControlState/test3.py
@@ -1,17 +1,19 @@
 import time
+from ControlState import ControlState
+cs = ControlState() 
 counter = 0 
 message_interval = 1
 last_message = 0
+cs.executePrograma("FRT;FRT;ESQ;ESQ;FRT;FRT")
 
-
-while counter < 10:
+while counter < 20:
   try:
  
     if (time.time() - last_message) > message_interval:
       counter += 1
       print(counter)
-      
- 
+      cs.updateExecution()
+      cs.showState()
       last_message = time.time()
       
   except OSError as e:
