3ffcf7a7c5c3517ad70cc63305db6aa679c53df4
Orivaldo Santana <orivaldo@gmail.com>
2022-06-21 11:14:06 -0300

    mqtt dht11

---
+++ ESP32/DHT11/main.py
@@ -0,0 +1,61 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+# Sensor de temperatura 
+from machine import Pin
+from time import sleep
+import dht
+
+sensor = dht.DHT11(Pin(14))
+
+ledPin = machine.Pin(4, machine.Pin.OUT)
+
+
+def sub_cb(topic, msg):
+  global topic_sub
+  print((topic, msg))
+  print(topic_sub)
+  if topic == topic_sub and msg == b'l':
+    print('ligar')
+    ledPin.on()
+    
+  if topic == topic_sub and msg == b'd':
+    print('desligar')
+    ledPin.off() 
+
+
+def connect_and_subscribe():
+  global client_id, mqtt_server, topic_sub, server_port, mqtt_user, mqtt_password
+  client = MQTTClient(client_id, mqtt_server, server_port, mqtt_user, mqtt_password)
+  client.set_callback(sub_cb)
+  client.connect()
+  client.subscribe(topic_sub)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
+
+try:
+  client = connect_and_subscribe()
+except OSError as e:
+  restart_and_reconnect()
+
+while True:
+  try:
+    client.check_msg()
+    if (time.time() - last_message) > message_interval:
+      sensor.measure()
+      temp = sensor.temperature()
+      msg = b'MSG %d %d' % (counter,temp) 
+      client.publish(topic_pub, msg)
+      last_message = time.time()
+      counter += 1
+  except OSError as e:
+    restart_and_reconnect()
+
+
+
+
+
